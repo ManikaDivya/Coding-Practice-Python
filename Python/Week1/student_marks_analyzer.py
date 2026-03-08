f5caebebab2b3c06d716f0f64af8906bab090c5c
ManikaDivya <manikadivya2005@gmail.com>
2026-03-08 11:38:17 +0530

    student_marks_analyzer.py
    This program takes marks of students as input and analyzes them. 
    It finds the highest mark, lowest mark, and average marks. 
    The program also counts pass and fail students and assigns grades based on their marks.

--- Python/Week1/student_marks_analyzer.py
+++ Python/Week1/student_marks_analyzer.py
@@ -1,5 +1,7 @@
 student_marks = list(map(int, input("Enter marks: ").split()))
 
+print("------ Student Marks Analysis ------")
+
 print(f"Highest Marks: {max(student_marks)}")
 print(f"Lowest Marks: {min(student_marks)}")
 
@@ -9,5 +11,34 @@ count = 0
 for mark in student_marks:
     total_marks += mark
     count += 1
+
+pass_count = 0
+fail_count = 0
+
+print("\nGrades:")
+
+for i in range(len(student_marks)):
+    mark = student_marks[i]
+
+    if mark >= 90:
+        grade = "A"
+        pass_count += 1
+    elif mark >= 75:
+        grade = "B"
+        pass_count += 1
+    elif mark >= 60:
+        grade = "C"
+        pass_count += 1
+    elif mark >= 50:
+        grade = "D"
+        pass_count += 1
+    else:
+        grade = "Fail"
+        fail_count += 1
+
+    print(f"Student {i+1}: {mark} → Grade {grade}")
+
+print(f"\nPass Students: {pass_count}")
+print(f"Fail Students: {fail_count}")
 
 print(f"Average Marks: {total_marks / count}")
